60967d2e07a92be6cdc5b0b9308b5057b26bb796
Touchet <gtouchet@myges.fr>
2022-05-24 13:50:29 +0200

    string comparer edition

--- Api/Utilitaries/StringComparer.cs
+++ Api/Utilitaries/StringComparer.cs
@@ -4,42 +4,52 @@ public class StringComparer
 {
     public double Compare(string source, string target)
     {
+        if (string.IsNullOrWhiteSpace(source.Trim()) || string.IsNullOrWhiteSpace(target.Trim()))
+        {
+            return 0.0;
+        }
+        if (source.Equals(target))
+        {
+            return 1.0;
+        }
+
         source = source.ToUpper();
         target = target.ToUpper();
 
-        if ((source == null) || (target == null)) return 0.0;
-        if ((source.Length == 0) || (target.Length == 0)) return 0.0;
-        if (source == target) return 1.0;
-
-        int stepsToSame = ComputeLevenshteinDistance(source, target);
-        return (1.0 - ((double)stepsToSame / (double)Math.Max(source.Length, target.Length)));
+        return 1.0 - (this.ComputeLevenshteinDistance(source, target) / (double)Math.Max(source.Length, target.Length));
     }
 
     private int ComputeLevenshteinDistance(string source, string target)
     {
-        if ((source == null) || (target == null)) return 0;
-        if ((source.Length == 0) || (target.Length == 0)) return 0;
-        if (source == target) return source.Length;
-
         int sourceWordCount = source.Length;
         int targetWordCount = target.Length;
 
         if (sourceWordCount == 0)
+        {
             return targetWordCount;
+        }
 
         if (targetWordCount == 0)
+        {
             return sourceWordCount;
+        }
 
         int[,] distance = new int[sourceWordCount + 1, targetWordCount + 1];
 
-        for (int i = 0; i <= sourceWordCount; distance[i, 0] = i++) ;
-        for (int j = 0; j <= targetWordCount; distance[0, j] = j++) ;
-
-        for (int i = 1; i <= sourceWordCount; i++)
+        for (int i = 0; i <= sourceWordCount; i += 1)
         {
-            for (int j = 1; j <= targetWordCount; j++)
+            distance[i, 0] = i;
+        }
+        for (int j = 0; j <= targetWordCount; j += 1)
+        {
+            distance[0, j] = j;
+        }
+
+        for (int i = 1; i <= sourceWordCount; i += 1)
+        {
+            for (int j = 1; j <= targetWordCount; j += 1)
             {
-                int cost = (target[j - 1] == source[i - 1]) ? 0 : 1;
+                int cost = target[j - 1].Equals(source[i - 1]) ? 0 : 1;
                 distance[i, j] = Math.Min(Math.Min(distance[i - 1, j] + 1, distance[i, j - 1] + 1), distance[i - 1, j - 1] + cost);
             }
         }
