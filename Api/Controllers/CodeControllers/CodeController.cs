28ce0ead1a5ad4537d5880d49ef9846f2c1be1cc
Touchet <gtouchet@myges.fr>
2022-06-03 12:17:39 +0200

    script execution dockerisation omfg

---
+++ Api/Controllers/CodeControllers/CodeController.cs
@@ -0,0 +1,13 @@
+﻿namespace Api.Controllers.CodeControllers;
+
+[ApiController]
+[Route("api/execute")]
+public partial class CodeController : ControllerBase
+{
+    private readonly IConfiguration configuration;
+
+    public CodeController(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+}
