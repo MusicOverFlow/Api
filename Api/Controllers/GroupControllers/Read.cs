ca95a318d1dbc148df639516ad9ccb096d3ae558
Touchet <gtouchet@myges.fr>
2022-07-24 13:30:29 +0200

    read all groups (no limit)

---
+++ Api/Controllers/GroupControllers/Read.cs
@@ -0,0 +1,20 @@
+﻿namespace Api.Controllers.GroupControllers;
+
+public partial class GroupController
+{
+    [HttpGet]
+    public async Task<ActionResult<GroupResource>> Read()
+    {
+        List<Group> groups = await this.context.Groups
+            .Include(g => g.Name)
+            .Include(g => g.Description)
+            .Include(g => g.PicUrl)
+            .ToListAsync();
+
+        List<GroupResource> groupResources = new List<GroupResource>();
+
+        groups.ForEach(g => groupResources.Add(this.mapper.Group_ToResource(g)));
+
+        return Ok(groupResources);
+    }
+}
