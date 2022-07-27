39aa2a5db38f5c8b4d7e69a1a7cf6a51d5ace01f
GTouchet <guillaumetouchet921@gmail.com>
2022-07-27 11:39:57 +0200

    list post again

--- Api/Controllers/PostControllers/ReadHomePage.cs
+++ Api/Controllers/PostControllers/ReadHomePage.cs
@@ -5,7 +5,7 @@ namespace Api.Controllers.PostControllers;
 public partial class PostController
 {
     [HttpGet("homePage"), AuthorizeEnum(Role.User, Role.Moderator, Role.Admin)]
-    public async Task<ActionResult<PostResource>> ReadHomePage()
+    public async Task<ActionResult<List<PostResource>>> ReadHomePage()
     {
         string mailAddress = this.User.Claims.FirstOrDefault(c => c.Type.Equals(ClaimTypes.Email)).Value;
 
