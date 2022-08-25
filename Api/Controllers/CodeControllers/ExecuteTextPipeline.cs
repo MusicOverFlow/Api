fd9b2d19ffe6ef9d98b4c359dcdf2ad550963570
Touchet <gtouchet@myges.fr>
2022-08-25 19:47:39 +0200

    fix text pipeline

--- Api/Controllers/CodeControllers/ExecuteTextPipeline.cs
+++ Api/Controllers/CodeControllers/ExecuteTextPipeline.cs
@@ -39,7 +39,7 @@ public partial class CodeController
                 FileName = "python",
                 Arguments =
                     $"{new DirectoryInfo(Directory.GetCurrentDirectory()).Parent}" +
-                    $"/PipelineScripts/Text/{script}.py " +
+                    $"/PipelineScripts/{script}.py " +
                     $"{filepath}",
                 RedirectStandardError = true,
                 RedirectStandardOutput = true,
